0590c2b79eadd6429ece7d30bff1869527c9c70a
Ionut Vasile <iovasile@proton.me>
2023-06-03 01:11:33 +0300

    Initial Commit

---
+++ shazam/shazamApp.swift
@@ -0,0 +1,17 @@
+//
+//  shazamApp.swift
+//  shazam
+//
+//  Created by Ionut Vasile on 03.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct shazamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
